cc24284c9762f53920ac00addf8ce45d8f52189d
engmung <lsh678902@gmail.com>
2024-12-15 16:27:10 +0900

    fix: API 경로 및 CORS 설정 원복

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'fort-net-frontend',
+      script: 'npm',
+      args: 'run preview',
+      cwd: './',
+      env: {
+        NODE_ENV: 'production',
+        VITE_API_URL: 'http://localhost:8000'
+      }
+    },
+    {
+      name: 'fort-net-backend',
+      cwd: './backend',
+      script: 'uvicorn',
+      args: 'main:app --host 0.0.0.0 --port 8000',
+      interpreter: './venv/bin/python3',
+      env: {
+        PYTHONPATH: '.',
+      }
+    }
+  ]
+};
